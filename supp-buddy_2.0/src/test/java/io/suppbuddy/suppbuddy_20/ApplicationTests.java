716129b8b3adf3b1032da3239f4befaabc9a3ab6
crv-9 <159793135+crv-9@users.noreply.github.com>
2024-07-25 18:08:02 +0200

    Add basic project structure and gitignore

---
+++ supp-buddy_2.0/src/test/java/io/suppbuddy/suppbuddy_20/ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.suppbuddy.suppbuddy_20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
